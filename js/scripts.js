1f70a7124eb0a81352b76822dc0fcd89c905c86b
Jasmine Gadie and Megan Lee <megan@>
2018-02-19 20:26:16 -0800

    changed page into a form to submit height and excitement with submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-  var height = parseInt(prompt("How tall are you?"));
+  $("form#userheight").submit(function(event) {
+  var height = parseInt($("input#height").val());
+  var excitement = parseInt($("input#excitement").val());
 
-  if (height === 65) {
+  if (height === 65 && excitement === 3) {
     $('#average-height').show();
-  } else if (height > 65) {
+  } else if (height > 65 && excitement < 3) {
     alert("You're too tall!");
     $('#tall-height').show();
   } else {
 //    (height < 65)
     $('#short-height').show();
   }
+    event.preventDefault();
+  });
 });
